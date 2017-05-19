a7973b126edfad83735948dfaedc42a695cb9395
Daryll <jarden0727@gmail.com>
2017-05-19 10:56:32 +0800

    modified the thymeleaf layout for building jar file - delete fragments folder - move all fragments inside templates folder -modified path in layout.html
    Signed-off-by: Chada0713 <jarden0727@gmail.com>

--- src/main/java/com/xenenergy/projects/KuryentxtWebsiteApplication.java
+++ src/main/java/com/xenenergy/projects/KuryentxtWebsiteApplication.java
@@ -2,8 +2,12 @@ package com.xenenergy.projects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @SpringBootApplication
 public class KuryentxtWebsiteApplication extends WebMvcConfigurerAdapter {
@@ -11,9 +15,22 @@ public class KuryentxtWebsiteApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(KuryentxtWebsiteApplication.class, args);
     }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/jasper/**").addResourceLocations("file:ext-resources/")
                 .setCachePeriod(0);
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("templates/");
+        //resolver.setSuffix(".html");
+        return resolver;
     }
 }
